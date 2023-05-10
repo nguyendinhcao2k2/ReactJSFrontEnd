00f234d9bf415a734e48b2e7ab937b16db5ed136
caodinh <caococ2002@gmail.com>
2023-05-10 07:50:08 +0700

    UpDATE

---
+++ src/utils/images.js
@@ -0,0 +1,10 @@
+import slider_img_1 from "../assets/images/slider1.jpg";
+import slider_img_2 from "../assets/images/slider_2.webp";
+import slider_img_3 from "../assets/images/slider_3.webp";
+import slider_img_4 from "../assets/images/slider_4.jpg";
+import shopping_cart from "../assets/images/shopping_cart.png";
+import correct from "../assets/images/correct.png";
+import loader from "../assets/images/loader.svg";
+
+const sliderImgs = [slider_img_1, slider_img_2, slider_img_3, slider_img_4];
+export { sliderImgs, shopping_cart, correct, loader };
